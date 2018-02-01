910b90027c31e9efdc4be12aad9ad0754d6bbf5d
Saith Kay <kaysaith@sina.com>
2018-02-01 19:08:40 +0800

    【功能】封装 ondraw 函数, 并重新实现首页布局

--- game.js
+++ game.js
@@ -20,8 +20,183 @@ let ScreenSize = {
 // 背景音乐
 let backgroundMusic = new Music()
 
+let logoImage = wx.createImage()
+logoImage.src = 'sources/image/wenqianLogo.png'
+
+let logoSize = 300
+let logoRect = {
+  left: (ScreenSize.width - logoSize) / 2,
+  top: (ScreenSize.height - logoSize) / 2 - 100,
+  width: logoSize,
+  height: logoSize
+}
+
+let titleColor = '#6f6351'
+
+
+let buttonLeft = ScreenSize.width * 0.2
+let buttonRect = {
+  top: ScreenSize.height - 200,
+  left: buttonLeft,
+  width: 162,
+  height: 80
+}
+
+let buttonBackgroundImage = wx.createImage()
+buttonBackgroundImage.src = 'sources/image/buttonBackground.png'
+
+let historyImage = wx.createImage()
+historyImage.src = 'sources/image/watchHistory.png'
+
+let destinyImage = wx.createImage()
+destinyImage.src = 'sources/image/requireDestiny.png'
+
+let redDotImage = wx.createImage()
+redDotImage.src = 'sources/image/redDot.png'
+
+let redDotSize = 30
+let redDotRect = {
+  width: redDotSize,
+  height: redDotSize,
+  left: (ScreenSize.width - redDotSize) / 2,
+  top: ScreenSize.height - 175
+}
+
+let branchImage = wx.createImage()
+branchImage.src = 'sources/image/branch.png'
+
+let branchSize = 480
+let branchRect = {
+  width: branchSize,
+  height: branchSize,
+  left: ScreenSize.width,
+  top: 30
+}
+
+let leafImage = wx.createImage()
+leafImage.src = 'sources/image/leaf.png'
+let leafSize = 66
+
+var branchMoveX = 0
+
+var leafY = 0
+var leafX = 0
+
 // 主界面
-new Main(canvas, ScreenSize)
+new Main(canvas, function(context, animation) {
+
+  drawBackground(context)
+  // 画 Logo
+  context.drawImage(
+    logoImage, 
+    logoRect.left, 
+    logoRect.top, 
+    logoRect.width, 
+    logoRect.height)
+  drawDescriptionText(context)
+
+  // 画按钮的背景笔墨
+  context.drawImage(
+    buttonBackgroundImage, 
+    buttonRect.left, 
+    buttonRect.top, 
+    buttonRect.width, 
+    buttonRect.height)
+
+  // 画寻史按钮
+  context.drawImage(
+    historyImage,
+    buttonRect.left,
+    buttonRect.top,
+    buttonRect.width,
+    buttonRect.height)
+
+  // 画求签的背景笔墨
+  context.drawImage(
+    buttonBackgroundImage,
+    ScreenSize.width - buttonLeft - buttonRect.width,
+    buttonRect.top,
+    buttonRect.width,
+    buttonRect.height)
+
+  // 画求签按钮
+  context.drawImage(
+    destinyImage,
+    ScreenSize.width - buttonLeft - buttonRect.width,
+    buttonRect.top,
+    buttonRect.width,
+    buttonRect.height)  
+
+  // 画按钮之间的红色小点
+  context.drawImage(
+    redDotImage,
+    redDotRect.left,
+    redDotRect.top,
+    redDotRect.width,
+    redDotRect.height
+  )
+
+  // 画树枝并做动画
+  branchMoveX += 5
+  context.drawImage(
+    branchImage,
+    branchRect.left - branchMoveX,
+    branchRect.top,
+    branchRect.width,
+    branchRect.height
+  )
+  if (branchMoveX >= branchSize) {
+    branchMoveX = branchSize
+    fallingLeaf()
+  }
+
+  function fallingLeaf() {
+    leafX += 1
+    leafY += 1
+    context.drawImage(
+      leafImage,
+      300 + leafX,
+      100 + leafY,
+      leafSize,
+      leafSize
+    )
+    if (leafY >= ScreenSize.height || leafX >= ScreenSize.width) {
+      leafY = 0
+      leafX = 0
+    }
+  }
+
+})
+
+function drawBackground(context) {
+  var gradient = context.createLinearGradient(0, 0, ScreenSize.width, ScreenSize.height)
+  gradient.addColorStop(0, '#dbd8d5')
+  gradient.addColorStop(0.5, '#ffffff')
+  gradient.addColorStop(1, '#eae8e6')
+
+  context.fillStyle = gradient
+  context.fillRect(0, 0, ScreenSize.width, ScreenSize.height)
+}
+
+function drawDescriptionText(context) {
+  context.fillStyle = titleColor
+  context.font = '24px avenir'
+  context.textBaseline = 'middle'
+  context.textAlign = 'center'
+  context.fillText(
+    'they determined that Cloyd had ovarian cysts',
+    ScreenSize.width / 2,
+    (ScreenSize.height + logoSize) / 2 - 50,
+    ScreenSize.width
+  )
+
+  context.fillText(
+    'gave her pain medications that helped her feel better',
+    ScreenSize.width / 2,
+    (ScreenSize.height + logoSize) / 2 - 50 + 36,
+    ScreenSize.width
+  )
+}
 
 // 后台恢复前台后继续播放背景音乐
 wx.onShow(function () {
--- module/home/home.js
+++ module/home/home.js
@@ -3,34 +3,20 @@
 * @date 2018-01-30
 */
 
-import Button from '../../common/homeButton'
-import LogoView from '../../common/homeLogoView'
-import CustomImage from '../../util/customImage'
-
-let redDotSize = 30
-let branchSize = 480
-
 export default class Main {
-  constructor(canvas, screenSize) {
-    this.context = canvas.getContext('2d')
-    this.logoView = new LogoView(screenSize, this.context)
-    this.watchHistoryButton = new Button(screenSize, this.context).initHistoryButton()
-    this.watchHistoryButton = new Button(screenSize, this.context).initDestinyButton()
+  constructor(canvas, onDraw) {
+    this.drawFrame(canvas, onDraw)
+  }
 
-    this.branchRect = {
-      width: branchSize,
-      height: branchSize,
-      left: screenSize.width - branchSize,
-      top: 30
+  drawFrame(canvas, onDraw) {
+    let context = canvas.getContext('2d')
+    drawDetail()
+    function drawDetail() {
+      let animation = requestAnimationFrame(drawDetail)
+      if (typeof onDraw === 'function') {
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        onDraw(context, animation)
+      }
     }
-    new CustomImage(this.context, this.branchRect).drawBranch()
-
-    this.redDotRect = {
-      width: redDotSize,
-      height: redDotSize,
-      left: (screenSize.width - redDotSize) / 2,
-      top: screenSize.height - 175
-    }
-    new CustomImage(this.context, this.redDotRect).drawRedDot()
   }
 }
